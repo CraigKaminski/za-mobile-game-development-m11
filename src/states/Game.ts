1f68d27b63cc5ab82abff6f8ce7f1de045029f92
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-01 18:38:03 -0500

    Hard code some buildings for testing

--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,8 +1,10 @@
+import { Building } from '../prefabs/Building';
 import { TownModel } from '../prefabs/TownModel';
 
 const Step = 2;
 
 export class Game extends Phaser.State {
+  private buildings: Phaser.Group;
   private simulationTimer: Phaser.TimerEvent;
   private town: TownModel;
 
@@ -14,10 +16,30 @@ export class Game extends Phaser.State {
     const background = this.add.tileSprite(0, 0, 1200, 800, 'grass');
     this.world.setBounds(0, 0, 1200, 800);
 
+    this.buildings = this.add.group();
+
+    const house = new Building(this, 100, 100, {
+      asset: 'house',
+      housing: 100,
+    });
+    this.buildings.add(house);
+
+    const farm = new Building(this, 200, 200, {
+      asset: 'crops',
+      food: 100,
+    });
+    this.buildings.add(farm);
+
+    const factory = new Building(this, 200, 300, {
+      asset: 'factory',
+      jobs: 20,
+    });
+    this.buildings.add(factory);
+
     this.town = new TownModel(
       undefined,
-      {population: 1, food: 200, money: 100, housing: 0, jobs: 0},
-      [{housing: 2}, {housing: 4}, {food: 50}, {jobs: 10}],
+      {population: 100, food: 200, money: 100, housing: 0, jobs: 0},
+      this.buildings,
     );
 
     this.simulationTimer = this.time.events.loop(Phaser.Timer.SECOND * Step, this.simulationStep, this);
